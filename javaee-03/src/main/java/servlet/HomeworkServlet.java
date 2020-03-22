62f15df1d972858f64f0f68ed927c3eafd5358a0
lovelycoder0 <1131606308@qq.com>
2020-03-22 09:23:35 +0800

    init javaee

---
+++ javaee-03/src/main/java/servlet/HomeworkServlet.java
@@ -0,0 +1,21 @@
+package servlet;
+
+import core.Homework;
+import jdbc.HomeworkJdbc;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+public class HomeworkServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        List<Homework> list= HomeworkJdbc.selectAll();
+
+        req.setAttribute("list",list);
+        req.getRequestDispatcher("teacher.jsp").forward(req,resp);
+    }
+}
